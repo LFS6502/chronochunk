2e5ab1d3f9ce33dbbe62158a0e6041254a5d1ad5
LFS6502 <lisafs6502@gmail.com>
2026-05-30 01:23:17 -0600

    Tellraw and polling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+#![allow(unused)]
+use std::any::type_name_of_val;
+
 use rcon::{AsyncStdStream, Connection, Error};
 
 #[tokio::main]
@@ -8,15 +11,27 @@ async fn main() -> Result<(), Error> {
         .connect(address, dotenv::var("password").unwrap().as_ref())
         .await?;
 
-    demo(&mut conn, "list").await?;
-    demo(&mut conn, "say Rust lang rocks! ;P").await?;
-    demo(&mut conn, "save-all").await?;
-    //demo(&mut conn, "stop");
+    //main check loop
+    loop {
+        let online = conn.cmd("list").await.unwrap();
+        let mut online = online
+            .trim()
+            .split_whitespace()
+            .filter_map(|item| item.parse::<i32>().ok());
+        println!("{}", online.next().unwrap());
+        tokio::time::sleep(std::time::Duration::from_secs(10)).await;
+        talk(&mut conn, "hello world :3").await;
+    }
     Ok(())
 }
-
 async fn demo(conn: &mut Connection<AsyncStdStream>, cmd: &str) -> Result<(), Error> {
     let resp = conn.cmd(cmd).await?;
     println!("{}", resp);
     Ok(())
+}
+
+async fn talk(conn: &mut Connection<AsyncStdStream>, text: &str) {
+    let cmd =
+        format!(r#"tellraw @a ["",{{"text":"[Spitfire] ","color":"red"}},{{"text":"{text}"}}]"#);
+    conn.cmd(&cmd).await.unwrap();
 }
