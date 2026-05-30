42be38fe0881542b4dbe02e8aff8eec3adfe48b5
LFS6502 <lisafs6502@gmail.com>
2026-05-30 02:24:16 -0600

    Parse gc/mem command into a struct

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,37 @@
 #![allow(unused)]
-use std::any::type_name_of_val;
-
+use parse_duration::parse;
 use rcon::{AsyncStdStream, Connection, Error};
+use std::{any::type_name_of_val, str::FromStr, time::Duration};
+use tokio::time::sleep;
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+struct ServerStatus {
+    uptime: Duration,
+    tps: f32,
+    mem_use: i32,
+    mem_alloc: i32,
+    mem_max: i32,
+    loaded_chunks: i32,
+}
+
+impl FromStr for ServerStatus {
+    type Err = (); //TODO: MAKE IT HAVE ERROR
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (left, right) = s.trim().split_once('|').unwrap();
+        let uptime_str = left.split_once("Uptime:").unwrap().1;
+        let duration = parse(uptime_str).unwrap();
+        let mut numbers = parse_numbers(right);
+        Ok(Self {
+            uptime: duration,
+            tps: numbers.next().unwrap(),
+            mem_use: numbers.next().unwrap() as i32,
+            mem_alloc: numbers.next().unwrap() as i32,
+            mem_max: numbers.next().unwrap() as i32,
+            loaded_chunks: numbers.next().unwrap() as i32,
+        })
+    }
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
@@ -14,13 +44,10 @@ async fn main() -> Result<(), Error> {
     //main check loop
     loop {
         let online = conn.cmd("list").await.unwrap();
-        let mut online = online
-            .trim()
-            .split_whitespace()
-            .filter_map(|item| item.parse::<i32>().ok());
-        println!("{}", online.next().unwrap());
-        tokio::time::sleep(std::time::Duration::from_secs(10)).await;
-        talk(&mut conn, "hello world :3").await;
+        let output = conn.cmd("gc").await.unwrap();
+        let status = output.parse::<ServerStatus>().unwrap();
+        dbg!(status);
+        sleep(Duration::from_hours(24)).await;
     }
     Ok(())
 }
@@ -34,4 +61,16 @@ async fn talk(conn: &mut Connection<AsyncStdStream>, text: &str) {
     let cmd =
         format!(r#"tellraw @a ["",{{"text":"[Spitfire] ","color":"red"}},{{"text":"{text}"}}]"#);
     conn.cmd(&cmd).await.unwrap();
+}
+
+fn parse_numbers(input: &str) -> impl Iterator<Item = f32> {
+    input
+        .trim()
+        .split(|c: char| c.is_whitespace() || c == '/')
+        .map(|word| {
+            word.chars()
+                .filter(|c| c.is_digit(10) || c == &'.')
+                .collect::<String>()
+        })
+        .filter_map(|item| item.parse::<f32>().ok())
 }
